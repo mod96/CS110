522cf64c73b0ff71acbe7ab68d23b5f2c8c76b6a
mod96 <58283520+mod96@users.noreply.github.com>
2023-01-02 21:32:03 +0900

    assign2 done

--- assign/assign2 - Reading Unix v6 File Systems/directory.c
+++ assign/assign2 - Reading Unix v6 File Systems/directory.c
@@ -13,5 +13,30 @@
  */
 int directory_findname(struct unixfilesystem *fs, const char *name, int dirinumber, struct direntv6 *dirEnt)
 {
-	return 0;
+	// check if it's directory
+	struct inode ino;
+	if (inode_iget(fs, dirinumber, &ino) < 0 || (ino.i_mode & IFMT) != IFDIR)
+		return -1;
+	// logic
+	int blockNum = 0;
+	char buf[DISKIMG_SECTOR_SIZE];
+	struct direntv6 *dir = (struct direntv6 *)buf;
+	int validSize, numEntriesInBlock;
+	while (1)
+	{
+		validSize = file_getblock(fs, dirinumber, blockNum, dir);
+		if (validSize <= 0)
+			break;
+		numEntriesInBlock = validSize / sizeof(struct direntv6);
+		for (int i = 0; i < numEntriesInBlock; i++)
+		{
+			if (strncmp(name, dir[i].d_name, strlen(name)) == 0)
+			{
+				*dirEnt = dir[i];
+				return 0;
+			}
+		}
+		blockNum++;
+	}
+	return -1;
 }
--- assign/assign2 - Reading Unix v6 File Systems/pathname.c
+++ assign/assign2 - Reading Unix v6 File Systems/pathname.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <malloc.h>
 
 #define DIR_MAX_LEN 14
 
@@ -16,5 +17,23 @@
  */
 int pathname_lookup(struct unixfilesystem *fs, const char *pathname)
 {
-	return 0;
+	// initialize directv6 to ROOT
+	struct direntv6 target;
+	target.d_inumber = ROOT_INUMBER;
+	// copy const char* to char* to use strtok
+	char *pName = malloc(strlen(pathname) + 1);
+	if (pName)
+		strcpy(pName, pathname);
+	// logic
+	char *p = strtok(pName, "/");
+	while (p)
+	{
+		int dirIno = target.d_inumber;
+		if (directory_findname(fs, p, dirIno, &target) < 0)
+		{
+			return -1;
+		}
+		p = strtok(NULL, "/");
+	}
+	return target.d_inumber;
 }
